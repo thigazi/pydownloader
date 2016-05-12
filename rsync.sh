3dbb86dd02e426fa20bfefa4d75a7bda4113ad31
Tamer Higazi <tamer@higazi-it.com>
2016-05-12 12:47:12 +0200

    not necessary

--- rsync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#rsync -z --update --delete-before -r --exclude=.* --exclude=*.pyc --exclude=dbx.fs --exclude=dbx.fs.* -avze ssh $PWD [2a02:d40:3:14::2]:/storage/PyProjects/bprojekte/
-rsync -z --update --delete-before -r --exclude=.* --exclude=*.pyc -avze ssh $PWD [2a02:d40:3:14::2]:/storage/PyProjects/bprojekte/
